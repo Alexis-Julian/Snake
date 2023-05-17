731bef65f929e946f640b6ac270169a04d50db29
Alexis <alexisjrojas@hotmail.es>
2023-05-17 05:55:45 -0300

    Complicaicones en agarrar puntos

--- Snake.js
+++ Snake.js
@@ -7,58 +7,76 @@ export default class SnakeManager {
     this.x = x;
     this.y = y;
     /* Puntos en movimiento */
-    this.points = 0;
-    /* this.break = { x: this.x - 20, y: this.y - 20 }; */
+    this.points = [{ x: this.x - 20, y: this.y }];
   }
-
   Snake(headSnake) {
     headSnake(this.x, this.y, 10, "green");
-    /* this.SpawnPoint(headSnake); */
     /*  this.points.map((p) => {
       headSnake(p.x, p.y, 10, "yellow");
     }); */
+    headSnake(this.points[0].x, this.points[0].y, 10, "blue");
   }
-
   movSnake() {
     let ultkey = null;
     let aux = null;
     document.addEventListener("keydown", (e) => {
       aux = ultkey;
       ultkey = e.keyCode;
+      if (ultkey != aux) this.MovPoints(ultkey);
     });
     setInterval(() => {
       if (ultkey === 37 && aux != 39) {
-        this.x = this.x - 1.0;
         /* KeyLeft */
+        this.x = this.x - 1.0;
+        this.points[0].x -= 1;
       } else if (ultkey === 39 && aux != 37) {
-        this.x = this.x + 1.0;
         /* KeyRight */
+        this.x = this.x + 1.0;
       } else if (ultkey === 38 && aux != 40) {
         /* KeyUp */
         this.y = this.y - 1.0;
       } else if (ultkey === 40 && aux != 38) {
         /* KeyDown */
         this.y = this.y + 1.0;
+        this.points[0].y += 1;
       } else {
         ultkey = aux;
       }
+
       this.ValidationPoint();
     }, 1000 / 60);
+    console.log("123");
+  }
+  MovPoints(ultkey) {
+    let c = 0;
+    this.points.map((e) => {
+      if (ultkey === 40) {
+        let radio = setInterval(() => {
+          e.x = e.x + 1;
+          e.y = e.y - 1;
+          c++;
+          if (c === 20) {
+            clearInterval(radio);
+          }
+        }, 1000 / 60);
+      } else if (ultkey === 37) {
+        let radio = setInterval(() => {
+          e.y += 1;
+          e.x += 1;
+          c++;
+          if (c === 20) {
+            clearInterval(radio);
+          }
+        }, 1000 / 60);
+      }
+    });
+    c = 0;
   }
   ValidationPoint() {
-    /*Centro : this.x,this.y  --- Punto : this.eat.ex  this.eat.ey   */
     let d = Math.sqrt(
       (this.eat.ex - this.x) ** 2 + (this.eat.ey - this.y) ** 2
     );
     if (d <= 10) {
-      console.log("Punto");
-
-      this.points += 1;
     }
   }
-  /* SpawnPoint(headSnake) {
-    let puntox = this.x;
-    let puntoy = this.y;
-    headSnake(puntox + 10, puntoy + 10, 10, "blue");
-  } */
 }
